c9e5a9c584c19fb4f5d2dd1decd1a8215eaeaed8
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-05-09 15:04:28 -0500

    configtest working to verify config format and latest backup date

--- configtest.py
+++ configtest.py
@@ -3,31 +3,32 @@
 from configparser import backup_parser, config_parser, arg_parser
 import sys
 
-#cparse = parser(sys.argv[1])
-#backups = dict()
-#while not cparse.empty():
-   #date = cparse.parseString()
-   #if date is None:
-      #break
-   #backup_list = cparse.parseBlock()
-   #if backup_list is None:
-      #break
-   #backups[date] = backup_list
-#bparse = backup_parser(sys.argv[1])
-#backups = bparse.read_backup_file()
+cparse = backup_parser(sys.argv[1])
+backups = dict()
+while not cparse.empty():
+   date = cparse.parseString()
+   if date is None:
+      break
+   backup_list = cparse.parseBlock()
+   if backup_list is None:
+      break
+   backups[date] = backup_list
+bparse = backup_parser(sys.argv[1])
+backups = bparse.read_backup_file()
 
-#or key in backups.keys():
-#   print (key + " {\n")
-#   for v in backups[key]:
-#      print (v + "\n")
-#   print ("}\n")
+for key in backups.keys():
+   print (key + " {\n")
+   for v in backups[key]:
+      print (v + "\n")
+   print ("}\n")
 
-options = dict()
+print max(backups.keys())
 
-cparse = config_parser("example-config")
+
+#cparse = config_parser("example-config")
 #print cparse.get_opt("exclude")
 #print cparse.get_opt("exclude", profile="kevin")
-aparse = arg_parser(sys.argv[1:], opt=cparse.get_options())
+#aparse = arg_parser(sys.argv[1:], opt=cparse.get_options())
 #aparse = arg_parser(sys.argv[1:])
-print aparse.options
-print aparse.options['exclude']
+#print aparse.options
+#print aparse.options['exclude']
