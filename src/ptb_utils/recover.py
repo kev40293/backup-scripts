321bfbe30a1fe9dce9d140d87d28c38ee3f936b2
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-05-22 19:53:32 +0200

    Recover now interactive to query which backup to restore from by the date.
    Fixes Issue #6

--- src/ptb_utils/recover.py
+++ src/ptb_utils/recover.py
@@ -26,13 +26,55 @@ def run(args):
    bparse = backup_parser(backup_file)
    recover_date = max(bparse.backups.keys())
    archives = bparse.backups[recover_date]
-   snarfile = backup_dir + "/" + name + '-' + recover_date + ".snar"
 
    oldcwd = os.getcwd()
    os.chdir(destination)
+   dlist = generate_datelist(bparse.backups)
+   for i,d in enumerate(dlist):
+      print str(i) + ") " + d
+   print ""
+   sys.stdout.write( "Select a date to restore from (0): ")
+   cin = sys.stdin.readline().rstrip('\n')
+   if cin == "":
+      choice = 0
+   else:
+      choice = int(cin)
+   recover_date = dlist[choice]
+
+   sys.stdout.write("Restore from " + recover_date + " (y/N) ")
+   cin = sys.stdin.readline().rstrip('\n')
+   if cin != "y":
+      return
+
+   snarfile = backup_dir + "/" + name + '-' + recover_date + ".snar"
+   for k in bparse.backups.keys():
+      if k <= recover_date:
+         base_back = k
+         break
+
+   archives = []
+   for arc in bparse.backups[base_back]:
+      if strip_date(arc) <= recover_date:
+         archives.append(arc)
+
+   archives.sort()
+   print archives
 
    for arc in archives:
       tar_args = ['tar', '-xjvf', backup_dir +"/" + arc, '-g', snarfile]
       call(tar_args)
 
    os.chdir(oldcwd)
+
+def strip_date(arcname):
+   return "-".join(arcname.split('-')[-3:]).rstrip('.tbz')
+
+def generate_datelist(backupdb):
+   datelist = []
+   for key in backupdb.keys():
+      for arc in backupdb[key]:
+         date = strip_date(arc)
+         datelist.append(date)
+   datelist.sort()
+   datelist.reverse()
+   return datelist
