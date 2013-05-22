5f24a5819ba6fe2336de0aca516a494bfec89373
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-05-22 16:09:32 +0200

    Base delete file, working selecting TODO: the actual deletion and integration into the master script

---
+++ src/ptb_utils/delete.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#      Copyright (C) 2013  Kevin Brandstatter <icarusthecow@gmail.com>
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+
+from configparser import backup_parser
+import sys, os
+from subprocess import call
+
+def run(args):
+   if len(args) != 2:
+      print "delete backup-file"
+      sys.exit(1)
+
+   backup_file=os.path.realpath(args[1])
+   backup_dir=os.path.dirname(backup_file)
+   name="".join(os.path.basename(backup_file).split('.')[0:-1])
+
+   bparse = backup_parser(backup_file)
+
+   backup_dates = bparse.backups.keys()
+   backup_dates.sort()
+   print backup_dates
+   for i,b in enumerate(backup_dates):
+      print str(i) + ') ' + b
+   print ""
+   sys.stdout.write("Select backup to delete (default 0): ")
+   ui = sys.stdin.readline().rstrip()
+   if ui == "":
+      choice = 0
+   else:
+      choice = int(ui)
+
+   sys.stdout.write("Deleting backup with date: " + backup_dates[choice] + " (y/N) ")
+   ui = sys.stdin.readline().lower().rstrip('\n')
+   if ui != 'y':
+      print "Canceling"
+      print ui
+      return
+   else:
+      print "Deleting..."
+   archives = bparse.backups[backup_dates[choice]]
+   snarfile = backup_dir + "/" + backup_dates[choice] + ".snar"
+
+   oldcwd = os.getcwd()
+   os.chdir(backup_dir)
+
+   for arc in archives:
+      print "rm " + arc
+   print "rm " + snarfile
+   print "Writing backup file"
+
+   os.chdir(oldcwd)
+
+run(sys.argv)
