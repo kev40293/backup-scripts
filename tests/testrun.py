700596a07b6c478b6d723e09f92d9b769436eee5
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-06-30 16:53:23 +0200

    Using loglevel DEBUG for test run

--- tests/testrun.py
+++ tests/testrun.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 from sys import path
 path.insert(0, '../src/')
+import logging
+logging.getLogger().setLevel("DEBUG")
 
 import imp
 imp.load_source('timeball', '../src/timeball')
+
