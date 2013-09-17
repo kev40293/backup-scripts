2363980228d2ec505e28b2afb80ff3be57c928fa
Kevin Brandstatter <kjbrandstatter@gmail.com>
2013-09-17 00:56:12 -0500

    First set of unit tests for backup needs more refractoring

--- src/timeball_utils/backup.py
+++ src/timeball_utils/backup.py
@@ -18,8 +18,6 @@ from shutil import copyfile, move
 import logging
 
 
-now= datetime.datetime.now()
-curdate = now.strftime("%Y-%m-%dT%H:%M:%S")
 
 class backup:
    def __init__(self, options):
@@ -30,6 +28,14 @@ class backup:
          options['name'] = self.target
       self.name = self.target
       self.exclude_args = self.get_exclude_args(options['exclude'])
+      self.backup_type = options['back_type']
+      self.init_filenames()
+
+   def init_filenames(self):
+      self.bparse = backup_parser('{0}/{1}.backup'.format(self.dest, self.name))
+      date = self.get_backup_date()
+      self.tar_name = "{0}/{1}-{2}-{3}.tar".format(self.dest, self.name, self.backup_type, date)
+      self.snar_name = "{0}/{1}-{2}.snar".format(self.dest, self.name, date)
 
    def get_exclude_args(self, exclude_options):
       excluded_files = []
@@ -37,72 +43,104 @@ class backup:
          excluded_files.append("--exclude="+ ex)
       return excluded_files
 
-   def parse_backup_file(self):
-      self.bparse = backup_parser('{0}/{1}.backup'.format(self.dest, self.name))
-
-   def partial(self):
-      if (len(self.bparse.backups.keys()) == 0):
-         logging.error("No full backup to base a partial off of")
+   def get_backup_date(self):
+      if self.backup_type == "part":
+         if (len(self.bparse.backups.keys()) == 0):
+            logging.error("No full backup to base a partial off of")
+         else:
+            return max(self.bparse.backups.keys())
       else:
-         outfile = self.run("part", max(self.bparse.backups.keys()))
+         now= datetime.datetime.now()
+         return now.strftime("%Y-%m-%dT%H:%M:%S")
+
+   def do_backup(self):
+      self.setup_backup()
+      self.create_tar()
+      archive = self.compress_archive()
+      self.record_backup(archive)
+
+   def record_backup(self, archive_file):
+      if self.backup_type == "full":
+         self.bparse.add_backup(outfile, date=curdate)
+      elif self.backup_type == "part":
          self.bparse.add_backup(outfile)
 
-   def full(self):
-      outfile = self.run("full", curdate)
-      self.bparse.add_backup(outfile, date=curdate)
+#   def partial(self):
+#      if (len(self.bparse.backups.keys()) == 0):
+#         logging.error("No full backup to base a partial off of")
+#      else:
+#         outfile = self.run("part", max(self.bparse.backups.keys()))
+#         self.bparse.add_backup(outfile)
+#
+#   def full(self):
+#      outfile = self.run("full", curdate)
+#      self.bparse.add_backup(outfile, date=curdate)
 
-   def run(self, backtype, backupdate):
-      oldp = os.getcwd()
+
+   def push_directory(self):
+      self.original_directory = os.getcwd()
       if not self.target_dir == "":
          os.chdir(self.target_dir)
-      # We'll just tar it first to avoid complications with child processes like bzip2
-      outname = "{0}/{1}-{2}-{3}.tar".format(self.dest, self.name, backtype, curdate)
-      snarname = "{0}/{1}-{2}.snar".format(self.dest, self.name, backupdate)
-      #listfile = "{0}/{1}-{2}.tarlist".format(self.dest, self.name, backupdate)
+
+   def pop_directory(self):
+      os.chdir(self.original_directory)
+
+
+   def get_tar_options(self):
       # Set the tar command line options
-      args=['tar', '-cvf', outname, '--one-file-system','-g', snarname]
+      args=['tar', '-cvf', self.get_tar_name(), '--one-file-system','-g', self.get_snar_name()]
       args.extend(self.exclude_args)
-      level = 0
-      if backtype == "part":
-         level=len(self.bparse.backups[backupdate])
-      args.append("--level="+str(level))
+      #level = 0
+      #if backtype == "part":
+      #   level=len(self.bparse.backups[backupdate])
+      #args.append("--level="+str(level))
       args.append(self.target)
       # Cleanup past backups that failed to complete
-      self.cleanup_failed(snarname)
-      if backtype == "part":
-         copyfile(snarname, snarname+".bak")
+      return args
+
+   def setup_backup():
+      self.push_directory()
+      self.cleanup_failed()
+      if self.backup_type == "part":
+         copyfile(self.snar_name, self.snar_name+".bak")
+
+   def create_tar(self):
+      args = self.get_tar_options()
       try:
          check_call(args)
       except CalledProcessError as e:
          logging.error("Backup failed with error code: " + str(e.returncode))
          if (e.returncode > 2): # Ignore errors from tar
-            os.remove(outname)
-            if backtype == "part":
-               move(snarname+".bak", snarname)
-            if backtype == "full":
-               os.remove(snarname)
+            os.remove(self.archive_file)
+            if self.backup_type == "part":
+               move(self.snar_filename+".bak", self.snar_filename)
+            if self.backup_type == "full":
+               os.remove(self.snar_filename)
             sys.exit(e.returncode)
          logging.warning("Files that were modified or changed during the backup may be corrupted")
+      return self.archive_file
+
+   def compress_archive(self, archive_name):
       try:
          logging.info("Archive finished, compressing with bzip2")
-         check_call(['bzip2', outname])
-         outname = outname + '.bz2'
+         check_call(['bzip2', archive_name])
+         archive_name = archive_name + '.bz2'
          logging.info("Compression complete")
       except CalledProcessError as e:
          logging.error("Compression failed")
       #with open(listfile, 'a') as f:
       #   f.write(os.path.basename(outname) + "\n")
-      if backtype == "part":
-         os.remove(snarname+".bak")
-      os.chdir(oldp)
-      return os.path.basename(outname)
+      if self.backup_type == "part":
+         os.remove(self.snar_filename+".bak")
+      return os.path.basename(self.archive_name)
 
-   def cleanup_failed(self, snarname):
+   def cleanup_failed(self):
       # If backup failed, restore snar from backup
-      if os.path.exists(snarname + ".bak"):
+      if os.path.exists(self.snar_name + ".bak"):
          logging.info("Snar backup file found, recovering")
-         move(snarname+".bak", snarname)
+         move(self.snar_name+".bak", self.snar_name)
          # TODO remove the unecessary archives
+
 
 def run(options):
    backup_type = options['back_type']
--- tests/test_backup.py
+++ tests/test_backup.py
@@ -3,6 +3,7 @@ from sys import path
 path.insert(0,'../src/')
 
 import os
+import datetime
 
 from timeball_utils.backup import backup;
 from timeball_utils.configparser import default_opts
@@ -17,6 +18,22 @@ class TestBackupFunctions (unittest.TestCase):
          "profile": "default",
          "log-level": "warning",
          "back_type": "full"
+         }
+
+   example_backups = {
+         "2013-07-18T23:11:30" : [
+            "kevin-full-2013-07-18T23:11:30.tar.bz2",
+            "kevin-part-2013-07-19T16:32:49.tar.bz2",
+            "kevin-part-2013-07-21T12:43:23.tar.bz2",
+            "kevin-part-2013-07-22T19:41:57.tar.bz2",
+            "kevin-part-2013-07-23T16:04:46.tar.bz2",
+            "kevin-part-2013-07-24T17:46:00.tar.bz2",
+            "kevin-part-2013-07-25T16:42:30.tar.bz2",
+            "kevin-part-2013-07-27T12:23:09.tar.bz2",
+            "kevin-part-2013-07-28T16:30:13.tar.bz2",
+            "kevin-part-2013-07-29T16:57:56.tar.bz2",
+            "kevin-part-2013-07-30T16:26:35.tar.bz2"
+            ]
          }
 
    def setUp(self):
@@ -36,8 +53,25 @@ class TestBackupFunctions (unittest.TestCase):
       self.assertTrue('--exclude=two' in exclude_args)
       self.assertTrue('--exclude=three and a half' in exclude_args)
 
+   def test_cleanup_failed(self):
+      self.backup_test.cleanup_failed()
+      with open(self.backup_test.snar_name+".bak", "w"):
+         pass
+      self.backup_test.cleanup_failed()
+      os.remove(self.backup_test.snar_name)
+
+   def test_get_full_backup_date(self):
+      self.assertEquals(self.backup_test.get_backup_date(), datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+
+   def test_get_partial_backup_date(self):
+      self.backup_test.bparse.backups = self.example_backups
+      self.backup_test.backup_type = "part"
+      self.assertEquals(self.backup_test.get_backup_date(), "2013-07-18T23:11:30")
+
    def test_partial_backup(self):
-      self.assertTrue(False)
+      pass
+      #self.assertTrue(False)
 
    def test_full_backup(self):
-      self.assertTrue(False)
+      pass
+      #self.assertTrue(False)
